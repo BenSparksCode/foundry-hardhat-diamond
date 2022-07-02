8d714d9c7f5918e7d0700590e05e902eb8d5ecc1
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-07-02 23:46:55 +0200

    Started on Foundry diamond deploy test

--- test/Diamond.t.sol
+++ test/Diamond.t.sol
@@ -3,16 +3,28 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
+import "../src/upgradeInitializers/DiamondInit.sol";
 import "../src/Diamond.sol";
+import "../src/facets/DiamondCutFacet.sol";
+import "../src/facets/DiamondLoupeFacet.sol";
+import "../src/facets/OwnershipFacet.sol";
 
 contract DiamondTest is Test {
+
     Diamond d;
+    DiamondInit di;
+    DiamondCutFacet dcf;
+    DiamondLoupeFacet dlf;
+    OwnershipFacet dof;
+
 
     function setUp() public {
         // d = new Diamond();
+        dcf = new DiamondCutFacet();
     }
 
-    function testName() public {
-        assertEq(true, true);
+    function testDeploy() public {
+        emit log_address(address(dcf));
+        assertTrue(address(dcf) != address(0));
     }
 }
